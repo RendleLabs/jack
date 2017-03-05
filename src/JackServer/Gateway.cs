b1bdf9e101b8a49ef33fef11208698f9f7f5eb4d
Mark Rendle <mark@rendle.io>
2017-03-05 22:53:16 +0000

    Proxying success, yay

--- src/JackServer/Gateway.cs
+++ src/JackServer/Gateway.cs
@@ -1,12 +1,19 @@
 using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
 
 namespace JackServer
 {
     internal class Gateway : IDisposable
     {
         private readonly IWebHost _host;
+        private readonly HttpClient _client;
 
         public Gateway(IConfigurationRoot config)
         {
@@ -14,7 +21,21 @@ namespace JackServer
             _host = new WebHostBuilder()
                 .UseUrls(gatewayUrls)
                 .UseKestrel()
+                .Configure(Configure)
                 .Build();
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri("http://localhost:5000");
+        }
+
+        private void Configure(IApplicationBuilder app)
+        {
+            app.Use(_ => async ctx =>
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, ctx.Request.Path);
+                var path = ctx.Request.Path;
+                var response = await _client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+                await CopyResponse(response, ctx.Response);
+            });
         }
 
         public void Start()
@@ -25,6 +46,28 @@ namespace JackServer
         public void Dispose()
         {
             _host.Dispose();
+        }
+
+        private static async Task CopyResponse(HttpResponseMessage from, HttpResponse to)
+        {
+            to.StatusCode = (int)from.StatusCode;
+            foreach (var kvp in from.Headers)
+            {
+                if (!kvp.Key.Equals("transfer-encoding", StringComparison.OrdinalIgnoreCase))
+                {
+                    System.Console.WriteLine($"{kvp.Key}: {string.Join(", ", kvp.Value)}");
+                    to.Headers.Add(kvp.Key, new StringValues(kvp.Value.ToArray()));
+                }
+            }
+            foreach (var kvp in from.Content.Headers)
+            {
+                if (!kvp.Key.Equals("transfer-encoding", StringComparison.OrdinalIgnoreCase))
+                {
+                    System.Console.WriteLine($"{kvp.Key}: {string.Join(", ", kvp.Value)}");
+                    to.Headers.Add(kvp.Key, new StringValues(kvp.Value.ToArray()));
+                }
+            }
+            await from.Content.CopyToAsync(to.Body);
         }
     }
 }
--- src/JackServer/Startup.cs
+++ src/JackServer/Startup.cs
@@ -40,7 +40,6 @@ namespace JackServer
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
             }
             else
             {
